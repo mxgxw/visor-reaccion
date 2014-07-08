5f41ec9a7144641c95cfbf180c568da73a0a42fc
Mario Gomez <mxgxw.alpha@gmail.com>
2014-07-07 23:25:15 -0600

    Initial commit. Proof of concept.

---
+++ fullScreen.js
@@ -0,0 +1,26 @@
+var             map = new OpenLayers.Map('map');
+            map.addControl(new OpenLayers.Control.LayerSwitcher());
+            
+            var gphy = new OpenLayers.Layer.Google(
+                "Google Physical",
+                {type: G_PHYSICAL_MAP}
+            );
+            var gmap = new OpenLayers.Layer.Google(
+                "Google Streets", // the default
+                {numZoomLevels: 20}
+            );
+            var ghyb = new OpenLayers.Layer.Google(
+                "Google Hybrid",
+                {type: G_HYBRID_MAP, numZoomLevels: 20}
+            );
+            var gsat = new OpenLayers.Layer.Google(
+                "Google Satellite",
+                {type: G_SATELLITE_MAP, numZoomLevels: 22}
+            );
+
+
+            map.addLayers([gphy, gmap, ghyb, gsat]);
+
+            map.setCenter(new OpenLayers.LonLat(10.2, 48.9), 5);
+
+map.addControl(new OpenLayers.Control.LayerSwitcher());
